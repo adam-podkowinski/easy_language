6b7728ff9d7a8abb90b2167a24d768db463fe40d
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-07-01 22:49:36 +0200

    Basic theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,23 +10,70 @@ class EasyLanguage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        primaryColor: Colors.purple,
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepPurple,
+        primaryColor: Colors.deepPurple,
         accentColor: Colors.lightGreenAccent,
         backgroundColor: Color(0xffC3DD94),
+        scaffoldBackgroundColor: Colors.white,
       ),
-      darkTheme: Theme.of(context).copyWith(backgroundColor: Colors.black87),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        primaryColor: Colors.deepPurple,
+        accentColor: Colors.lightGreenAccent,
+        backgroundColor: Colors.black12,
+        scaffoldBackgroundColor: Colors.black12,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Easy Language'),
         ),
         body: Container(
-          color: Color(0xffC3DD94),
           child: Center(
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('Hello'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'How are you doing!',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'I bet you are doing well!',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 200,
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
