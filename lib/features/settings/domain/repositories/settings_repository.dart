62ad180bc325acf92507c603fd3283acd033f7f9
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-07-02 23:53:44 +0200

    Add settings repository and use case

---
+++ lib/features/settings/domain/repositories/settings_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:easy_language/core/error/failures.dart';
+import 'package:easy_language/features/settings/domain/entities/settings.dart';
+import 'package:flutter/material.dart';
+
+abstract class SettingsRepository {
+  Future<Either<Failure, Settings>> changeSettings({
+    bool? isStartup,
+    ThemeMode? themeMode,
+  });
+}
---
+++ lib/features/settings/domain/use_cases/change_settings.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:easy_language/core/error/failures.dart';
+import 'package:easy_language/core/use_cases/use_case.dart';
+import 'package:easy_language/features/settings/domain/entities/settings.dart';
+import 'package:easy_language/features/settings/domain/repositories/settings_repository.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class ChangeSettings implements Usecase<Settings, SettingsParams> {
+  final SettingsRepository repository;
+
+  ChangeSettings(this.repository);
+
+  Future<Either<Failure, Settings>> call(SettingsParams params) async {
+    return await repository.changeSettings(
+      isStartup: params.isStartup,
+      themeMode: params.themeMode,
+    );
+  }
+}
+
+class SettingsParams extends Equatable {
+  final bool? isStartup;
+  final ThemeMode? themeMode;
+
+  SettingsParams({this.isStartup, this.themeMode});
+
+  @override
+  List<Object?> get props => [isStartup, themeMode];
+}
